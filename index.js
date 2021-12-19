b7e1a13bf1e53c0c07c80853ebf862be812b7c6b
zarif007 <zarifhuq786@gmail.com>
2021-12-19 18:40:55 +0600

    Installed mysql, added middlewares

--- index.js
+++ index.js
@@ -2,9 +2,22 @@ const express = require('express');
 const app = express();
 require('dotenv').config();
 const cors = require('cors');
+const mysql = require('mysql');
+
+app.use(cors());
+app.use(express.json());
+
+const db = mysql.createConnection({
+    user: process.env.DB_USER,
+    host: 'localhost',
+    password: process.env.DB_PASS,
+    database: 'hail_online',
+});
 
 
 const port = process.env.PORT || 5000;
+
+
 
 app.listen(port, () => {
     console.log('ok!')
