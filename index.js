cd8e47694101cf3f084459a34bfa8938e02b95e9
zarif007 <zarifhuq786@gmail.com>
2021-12-20 01:22:49 +0600

    Added exam, createexam endpoints

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 require('dotenv').config();
 const cors = require('cors');
-const mysql = require('mysql');
+const mysql = require('mysql2');
 
 app.use(cors());
 app.use(express.json());
@@ -17,6 +17,36 @@ const db = mysql.createConnection({
 
 const port = process.env.PORT || 5000;
 
+
+app.post('/makeexam', (req, res) => {
+    const {exam_id, name, subject, assign_date, last_date} = req.body;
+
+    db.query(
+        "INSERT INTO exams (exam_id, name, subject, assign_date, last_date) VALUES(?,?,?,?,?)",
+        [exam_id, name, subject, assign_date, last_date],
+        (err, result) => {
+            if(err)
+                console.log(err);
+            else 
+                res.send('Values Inserted')
+        }
+    )
+});
+
+
+app.get('/exams/:id', (req, res) => {
+    const id = req.params.id;
+
+    db.query(
+        `SELECT * from exams WHERE exam_id="${id}"`,
+        (err, result) => {
+            if(err)
+                console.log(err);
+            else 
+                res.send(result);
+        }
+    )
+})
 
 
 app.listen(port, () => {
