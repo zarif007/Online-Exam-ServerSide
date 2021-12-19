55a064ce6cb10466d4190e7cf92125dccb1c3601
zarif007 <zarifhuq786@gmail.com>
2021-12-20 03:57:08 +0600

    Added questions endpoints

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const mysql = require('mysql2');
 app.use(cors());
 app.use(express.json());
 
+
+// Connecting to MySQL database
 const db = mysql.createConnection({
     user: process.env.DB_USER,
     host: 'localhost',
@@ -18,6 +20,7 @@ const db = mysql.createConnection({
 const port = process.env.PORT || 5000;
 
 
+// Storing Exam info
 app.post('/makeexam', (req, res) => {
     const {exam_id, name, subject, assign_date, last_date} = req.body;
 
@@ -33,8 +36,8 @@ app.post('/makeexam', (req, res) => {
     )
 });
 
-
-app.get('/exams/:id', (req, res) => {
+// Getting exam info
+app.get('/exam/:id', (req, res) => {
     const id = req.params.id;
 
     db.query(
@@ -46,7 +49,41 @@ app.get('/exams/:id', (req, res) => {
                 res.send(result);
         }
     )
+});
+
+// Storing Questions
+app.post('/addquestion', (req, res) => {
+
+    const {exam_id, ques_id, question, option1, option2, option3, option4, answer} = req.body;
+
+    db.query(
+        "INSERT INTO questions (exam_id, ques_id, question, option1, option2, option3, option4, answer) VALUES(?,?,?,?,?,?,?,?)",
+        [exam_id, ques_id, question, option1, option2, option3, option4, answer],
+        (err, result) => {
+            if(err)
+                console.log(err);
+            else 
+                res.send('Values Inserted')
+        }
+    )
+});
+
+// Getting Question
+app.get('/question/:id', (req, res) => {
+    const id = req.params.id;
+
+    db.query(
+        `SELECT * from questions WHERE exam_id="${id}"`,
+        (err, result) => {
+            if(err)
+                console.log(err);
+            else 
+                res.send(result);
+        }
+    )
 })
+
+
 
 
 app.listen(port, () => {
