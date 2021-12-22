7453a979c52ead48d38d74fba5753100503924ee
zarif007 <zarifhuq786@gmail.com>
2021-12-23 03:51:38 +0600

    Added user's grades endpoint

--- index.js
+++ index.js
@@ -86,11 +86,11 @@ app.get('/questions/:id', (req, res) => {
 // Storing user's response
 app.post('/response', (req, res) => {
 
-    const { ques_id, user_id, answer, userAnswer } = req.body;
+    const { exam_id, ques_id, user_id, answer, userAnswer } = req.body;
 
     db.query(
-        "INSERT INTO response (ques_id, user_id, answer, userAnswer) VALUES(?,?,?,?)",
-        [ques_id, user_id, answer, userAnswer],
+        "INSERT INTO response (exam_id, ques_id, user_id, answer, userAnswer) VALUES(?,?,?,?,?)",
+        [exam_id, ques_id, user_id, answer, userAnswer],
         (err, result) => {
             if(err)
                 console.log(err);
@@ -100,6 +100,71 @@ app.post('/response', (req, res) => {
     )
 });
 
+// Getting ques based user's response
+app.get('/questions/:quesId/:userId', (req, res) => {
+    
+    const ques_id = req.params.quesId, user_id = req.params.userId;
+
+    db.query(
+        `SELECT * from response WHERE ques_id="${ques_id}" AND user_id="${user_id}"`,
+        (err, result) => {
+            if(err)
+                console.log(err);
+            else 
+                res.send(result);
+        }
+    )
+});
+
+// Getting user's grade
+app.get('/grades/:exam_id/:user_id', (req, res) => {
+
+    const exam_id = req.params.exam_id, user_id = req.params.user_id;
+
+    let data = {};
+
+    db.query(
+        `SELECT * from exams WHERE exam_id="${exam_id}"`,
+        (err, result) => {
+            if(err)
+                console.log(err);
+            else {
+                data = {
+                    name: result[0].name,
+                    subject: result[0].subject,
+                    author: result[0].author,
+                }
+
+                db.query(
+                    `SELECT * from questions WHERE exam_id="${exam_id}"`,
+                    (err, result) => {
+                        if(err)
+                            console.log(err);
+                        else {
+                            let total_ques = result.length, currect_answer = 0;
+                            
+                            db.query(
+                                `SELECT * from response WHERE exam_id="${exam_id}" AND user_id="${user_id}"`,
+                                (err, result) => {
+                                    if(err)
+                                        console.log(err);
+                                    else {
+                                        data.total_ques = total_ques;
+                                        result.map(res => currect_answer += res.answer === res.userAnswer);
+                                        
+                                        data.currect_answer = currect_answer;
+
+                                        res.send(data);
+                                    }
+                                }
+                            )
+                        }
+                    }
+                );
+            }
+        }
+    );
+})
 
 
 
